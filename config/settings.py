ecbc40e096d662dbebd605a2bf44b76e0fd42d75
wynemo <ggdabin@gmail.com>
2012-05-30 14:27:44 +0000

    add dictmd submodule,change config setting

--- config/settings.py
+++ config/settings.py
@@ -6,12 +6,14 @@ render = web.template.render('templates/', cache=False)
 
 web.config.debug = False
 
-
+consumer_key = ""
+consumer_secret = ""
 std_headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.12) Gecko/20101028 Firefox/3.6.12',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language': 'en-us,en;q=0.5',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2',
+    #'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-us,en;q=0.5'
                 }
                 
 # dropbox_url = 'http://dl.dropbox.com/u/13112361/'  
