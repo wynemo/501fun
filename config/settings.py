bab57a61aa7e3bb2b04497b10db6399238501ede
wynemo <ggdabin@gmail.com>
2012-05-31 13:57:43 +0000

    add submodule twitter api

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 import web
+import os
 
-render = web.template.render('templates/', cache=False)
+def get_home_dir():
+    try:
+        dir1 = os.environ['OPENSHIFT_REPO_DIR']
+        if not dir1.endswith('/'):
+            dir1 += '/'
+        dir1 += 'wsgi/'
+        return dir1
+    except:
+        return './'
+
+render = web.template.render(get_home_dir() + 'templates/', cache=False)
 
 web.config.debug = False
 
 consumer_key = ""
 consumer_secret = ""
+
+#for j.mp link
+no_jmp = True
+bitly_name = ''
+bitly_key = ''
+
 std_headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2',
     #'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
@@ -16,4 +33,4 @@ std_headers = {
     'Accept-Language': 'en-us,en;q=0.5'
                 }
                 
-# dropbox_url = 'http://dl.dropbox.com/u/13112361/'  
+
